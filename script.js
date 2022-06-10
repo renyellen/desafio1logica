6ae5978433b3bb931f748e7a8ecad23d0e28f5e4
renyellen <reny1934@gmail.com>
2022-06-09 23:18:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let string="Renyellen";
+let numero="25";
+let anoAtual="2022";
+let operacao = anoAtual - numero;
+console.log(operacao);
+console.log("Estou mexendo no console.log");
+let quartaFeira = true;
+console.log(typeof quartaFeira);
