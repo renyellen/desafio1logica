618880e94e76c9188b111445bfec970bcc1af50e
renyellen <reny1934@gmail.com>
2022-06-24 11:58:30 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let string="Renyellen";
 let numero="25";
 let anoAtual="2022";
+console.log(anoAtual)
+let soma = anoAtual + numero;
+console.log(soma)
 let operacao = anoAtual - numero;
 console.log(operacao);
 console.log("Estou mexendo no console.log");
 let quartaFeira = true;
+console.log(quartaFeira)
 console.log(typeof quartaFeira);
